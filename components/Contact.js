163cbb2dc94adf931dc5ec0917b155ce4da268d6
smdd198 <shalimarmehra1010@gmail.com>
2025-07-29 20:35:20 +0530

    commit

--- components/Contact.js
+++ components/Contact.js
@@ -74,10 +74,10 @@ const Contact = () => {
                   />
                   Email-me :&nbsp;
                   <a
-                    href="mailto:contactshalimarmehra@gmail.com"
+                    href="mailto:shalimarmehra9213@gmail.com"
                     className="underline flex items-center"
                   >
-                    contactshalimarmehra@gmail.com
+                    shalimarmehra9213@gmail.com
                     <FaExternalLinkAlt className="ml-2" />
                   </a>
                 </b>
